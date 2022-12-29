f4431c526896740bcfc1cda5982d957c216457cd
sbaranchuk <sbaranchuk@protonmail.com>
2022-12-29 19:05:12 +0300

    Initial Commit

---
+++ Dz12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dz12
+//
+//  Created by Admin on 29/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
